76df0b21dcb159d543ef82e00f2d4728d78172a5
Necati Arslan <necati.arslan2018@gmail.com>
2025-07-25 12:11:17 +0200

    init commit

---
+++ src/test/java/com/example/spring_quick_start_project/SpringQuickStartProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_quick_start_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringQuickStartProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
